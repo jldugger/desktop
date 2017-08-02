e3fe7ffe7ec38bba108dd9d59c567df4d12c1cdb
Justin Dugger <jldugger@gmail.com>
2017-08-02 16:13:41 -0700

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jldugger@gmail.com'
 license 'GPL-2.0'
 description 'Installs/Configures workstation'
 long_description 'Installs/Configures workstation'
-version '0.2.6'
+version '0.2.7'
 
 chef_version '>= 12.5' if respond_to?(:chef_version)
 supports 'ubuntu'
