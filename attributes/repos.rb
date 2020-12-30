9217225905b1489080bc06b5d41013d4a123afb9
Justin Dugger <jldugger@gmail.com>
2020-12-29 16:15:13 -0800

    remove opera repo

--- attributes/repos.rb
+++ attributes/repos.rb
@@ -1,10 +1,4 @@
 default['desktop']['repos'] = {
-  'opera' => {
-    'uri' => 'http://deb.opera.com/opera/',
-    'distribution' => 'stable',
-    'components' => ['non-free'],
-    'key' => 'http://deb.opera.com/archive.key'
-  },
   'google-chrome' => {
     'arch' => 'amd64',
     'uri' => 'http://dl.google.com/linux/chrome/deb',
